1857c160264ec6df66926119ca71fedea336a37b
marksymonm <marksymonmartinez05@gmail.com>
2026-07-16 11:39:14 +0800

    redesigned portfoilio using react and typescript

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="main">
+      <h1>🚧 Work in Progress</h1>
+      <p>
+        I'm currently redesigning my portfolio using React + TypeScript.
+      </p>
+      <span>For now, this is a design preview only.</span>
+    </main>
+  );
+}
+
+export default App;
